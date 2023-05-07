e1565d67316fce747d1865f41669a2d03bb138b2
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-05-07 13:27:39 -0400

    Fixed Jumping for player, made it so that you can change the height, gravity, and amount of times player can jump.

--- Merry Mushrooms/Assets/Scripts/PlayerController.cs
+++ Merry Mushrooms/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private Vector3 move;
+    private int jumpedTimes;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    [Header("----- Player Stats -----")]
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
+    [Range(1, 3)][SerializeField] int maxJumps;
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
@@ -27,23 +30,26 @@ public class PlayerController : MonoBehaviour
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+            jumpedTimes = 0;
         }
 
-        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = (transform.right * Input.GetAxis("Horizontal")) +
+               (transform.forward * Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        //if (move != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = move;
+        //}
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetButtonDown("Jump") && jumpedTimes < maxJumps)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            jumpedTimes++;
+            playerVelocity.y = jumpHeight;
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        playerVelocity.y -= gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
