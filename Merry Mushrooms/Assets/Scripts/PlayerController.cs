d6d956cd6dd217186e1b38b7593bb6055b39f191
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-05-09 17:17:14 -0400

    Reactivated Spawn Function for Player

--- Merry Mushrooms/Assets/Scripts/PlayerController.cs
+++ Merry Mushrooms/Assets/Scripts/PlayerController.cs
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
         //controller = gameObject.AddComponent<CharacterController>();
         origSpeed = playerSpeed;
-        //Spawn();
+        Spawn();
     }
 
     void Update()
