53a3773da73f219727477e539587557aa3223980
Cmonty417 <118748396+Cmonty417@users.noreply.github.com>
2023-05-09 13:00:51 -0400

    Created Bullet Script

--- Merry Mushrooms/Assets/Scripts/Bullet.cs
+++ Merry Mushrooms/Assets/Scripts/Bullet.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class bullet : MonoBehaviour
 {
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int timer;
+
+    [SerializeField] Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, timer);
+        rb.velocity = transform.forward * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        {
+            IDamage damagable = other.GetComponent<IDamage>();
+
+            if (damagable != null)
+            {
+                damagable.takeDamage(damage);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
