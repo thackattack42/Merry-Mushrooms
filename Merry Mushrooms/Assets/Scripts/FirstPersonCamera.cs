6d7da1000632424d837907db98773f93ece39d8f
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-05-08 22:09:25 -0400

    Fixed an issue with camera where it would not move the player along with it.

--- Merry Mushrooms/Assets/Scripts/FirstPersonCamera.cs
+++ Merry Mushrooms/Assets/Scripts/FirstPersonCamera.cs
@@ -9,14 +9,14 @@ public class FirstPersonCamera : MonoBehaviour
     public float sensY;
 
     public Transform orientation;
-
+    public GameObject obj;
     float xRotation;
     float yRotation;
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -25,14 +25,17 @@ public class FirstPersonCamera : MonoBehaviour
        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
+        
         yRotation += mouseX;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -80, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);
 
         //rotate cam and orientation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //orientation.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.parent.Rotate(Vector3.up * mouseX);
+        //obj.transform.parent.Rotate(Vector3.up * mouseX);
 
     }
 }
