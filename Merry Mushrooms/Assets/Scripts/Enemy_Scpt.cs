6dcd9c97d84c1db770eb1780c163bd3a2ed67969
TrinitySanders <118745562+TrinitySanders@users.noreply.github.com>
2023-05-11 01:50:52 -0400

    shoot rate fixed

--- Merry Mushrooms/Assets/Scripts/Enemy_Scpt.cs
+++ Merry Mushrooms/Assets/Scripts/Enemy_Scpt.cs
@@ -20,7 +20,7 @@ public class Enemy_Scpt : MonoBehaviour, IDamage
 
     [Header("------ Enemy Weapon Stats ------")]
     [Range(5, 10)][SerializeField] int shootDist;
-    [Range(1, 10)][SerializeField] float ShootRate;
+    [Range(0.1f, 10)][SerializeField] float ShootRate;
     [Range(30, 180)][SerializeField] float ShootAngle;
     [SerializeField] GameObject bullet;
     //
