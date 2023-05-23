d47ea8dfdc5aac30614127441da5ffc4cf717529
Chase Thacker <91438585+thackattack42@users.noreply.github.com>
2023-05-23 11:47:00 -0400

    implemented ice enemy taking more damage from fire staff (not fully done)

--- Merry Mushrooms/Assets/Scripts/IceEnemy_Scpt.cs
+++ Merry Mushrooms/Assets/Scripts/IceEnemy_Scpt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceEnemy_Scrpt : Enemy_Scpt, IFireDamage
+public class IceEnemy_Scpt : Enemy_Scpt, IFireDamage
 {
     public void TakeFireDamage(int dmg)
     {
