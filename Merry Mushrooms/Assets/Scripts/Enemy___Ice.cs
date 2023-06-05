7fddc84b6b5441f1bd79e92fbf41c87d543bad40
TrinitySanders <118745562+TrinitySanders@users.noreply.github.com>
2023-06-05 14:51:04 -0400

    pushed iceweak change

--- Merry Mushrooms/Assets/Scripts/Enemy___Ice.cs
+++ Merry Mushrooms/Assets/Scripts/Enemy___Ice.cs
@@ -2,17 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy___Ice : MonoBehaviour
+public class Enemy___Ice : MonoBehaviour, IFireDamage, IEarthDamage, IIceDamage, IDamage
 {
-    // Start is called before the first frame update
-    void Start()
+    Enemy_Scpt enemy;
+
+    private void Start()
     {
-        
+        enemy = GetComponent<Enemy_Scpt>();
+    }
+    public void TakeFireDamage(int dmg)
+    {
+        enemy.HP -= dmg * 2;
+
+        if (enemy.HP <= 0)
+        {
+            enemy.animr.SetBool("Death", true);
+            enemy.agent.enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+            StartCoroutine(enemy.EnemyDespawn());
+        }
+        else
+        {
+            enemy.animr.SetTrigger("Damaged");
+            enemy.agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(enemy.FlashHitColor());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeEarthDamage(int dmg)
     {
-        
+        enemy.HP -= dmg;
+        if (enemy.HP <= 0)
+        {
+            enemy.animr.SetBool("Death", true);
+            enemy.agent.enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+            StartCoroutine(enemy.EnemyDespawn());
+        }
+        else
+        {
+            enemy.animr.SetTrigger("Damaged");
+            enemy.agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(enemy.FlashHitColor());
+        }
+    }
+
+    public void TakeIceDamage(int dmg)
+    {
+        enemy.HP += dmg;
+    }
+
+    public void takeDamage(int dmg)
+    {
+        enemy.HP -= dmg;
+
+        if (enemy.HP <= 0)
+        {
+            enemy.animr.SetBool("Death", true);
+            enemy.agent.enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+            StartCoroutine(enemy.EnemyDespawn());
+        }
+        else
+        {
+            enemy.animr.SetTrigger("Damaged");
+            enemy.agent.SetDestination(gameManager.instance.player.transform.position);
+            StartCoroutine(enemy.FlashHitColor());
+        }
     }
 }
