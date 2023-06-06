6e7c8aeb47ae7e5841031edf509c69a7caaeff5f
Cmonty417 <118748396+Cmonty417@users.noreply.github.com>
2023-06-06 00:37:19 -0400

    added treasure chest
    still in the making

--- Merry Mushrooms/Assets/Scripts/LootBox.cs
+++ Merry Mushrooms/Assets/Scripts/LootBox.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
 
 public class LootBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [CreateAssetMenu]
 
-    // Update is called once per frame
-    void Update()
+    public class Loot : ScriptableObject
     {
-        
-    }
+        public GameObject LootPrefab;
+        public List<Loot> lootList = new List<Loot>();
+        public Sprite lootSprite;
+        public string lootName;
+        public int dropChance;
+
+        void Start()
+        {
+            lootList.Add(new Loot("FunGil", 100));
+        }
+
+        public Loot(string lootName, int dropChance)
+        {
+            this.lootName = lootName;
+            this.dropChance = dropChance;
+        }
+
+        Loot GetLootBox()
+        {
+            int randomNumber = Random.Range(1, 101);
+            List<Loot> possibleLoot = new List<Loot>();
+
+            foreach(Loot item in lootList)
+            {
+                if (randomNumber <= item.dropChance)
+                {
+                    possibleLoot.Add(item);
+                    return item;
+                }
+            }
+            if (possibleLoot.Count > 0)
+            {
+                Loot loot = possibleLoot[Random.Range(0, possibleLoot.Count)];
+                return loot;
+            }
+            Debug.Log("No Loot Dropped");
+            return null;
+        }
+
+        public void InstantiateLoot(Vector3 spawnPos)
+        {
+            Loot possibleLoot = GetLootBox();
+            if (possibleLoot != null)
+            {
+                GameObject LootGameObject = Instantiate(LootPrefab, spawnPos, Quaternion.identity);
+                LootGameObject.GetComponent<SpriteRenderer>().sprite = possibleLoot.lootSprite;
+            }
+        }
+    }  
 }
