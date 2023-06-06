6268c87ae95d6741a153d1eea50c3ec26b13a6d6
Cmonty417 <118748396+Cmonty417@users.noreply.github.com>
2023-06-06 19:39:19 -0400

    Chest box complete

--- Merry Mushrooms/Assets/Scripts/LootBox.cs
+++ Merry Mushrooms/Assets/Scripts/LootBox.cs
@@ -3,61 +3,50 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-
-
 public class LootBox : MonoBehaviour
 {
-    [CreateAssetMenu]
+    public List<Loot> lootList;
 
-    public class Loot : ScriptableObject
+    void Start()
     {
-        public GameObject LootPrefab;
-        public List<Loot> lootList = new List<Loot>();
-        public Sprite lootSprite;
-        public string lootName;
-        public int dropChance;
+    }
 
-        void Start()
+    Loot GetDroppedItem()
+    {
+        int randomNumber = Random.Range(1, 101);
+        List<Loot> possibleLoot = new List<Loot>();
+
+        foreach (Loot item in lootList)
         {
-            lootList.Add(new Loot("FunGil", 100));
-        }
-
-        public Loot(string lootName, int dropChance)
-        {
-            this.lootName = lootName;
-            this.dropChance = dropChance;
-        }
-
-        Loot GetLootBox()
-        {
-            int randomNumber = Random.Range(1, 101);
-            List<Loot> possibleLoot = new List<Loot>();
-
-            foreach(Loot item in lootList)
+            if (randomNumber <= item.dropChance)
             {
-                if (randomNumber <= item.dropChance)
-                {
-                    possibleLoot.Add(item);
-                    return item;
-                }
-            }
-            if (possibleLoot.Count > 0)
-            {
-                Loot loot = possibleLoot[Random.Range(0, possibleLoot.Count)];
-                return loot;
-            }
-            Debug.Log("No Loot Dropped");
-            return null;
-        }
-
-        public void InstantiateLoot(Vector3 spawnPos)
-        {
-            Loot possibleLoot = GetLootBox();
-            if (possibleLoot != null)
-            {
-                GameObject LootGameObject = Instantiate(LootPrefab, spawnPos, Quaternion.identity);
-                LootGameObject.GetComponent<SpriteRenderer>().sprite = possibleLoot.lootSprite;
+                possibleLoot.Add(item);
             }
         }
-    }  
+        if (possibleLoot.Count > 0)
+        {
+            Loot droppedItem = possibleLoot[Random.Range(0, possibleLoot.Count)];
+            return droppedItem;
+        }
+        Debug.Log("No Loot Dropped");
+        return null;
+    }
+
+    public void InstantiateLoot(Vector3 spawnPos)
+    {
+        Loot droppedItem = GetDroppedItem();
+        if (droppedItem != null)
+        {
+            GameObject lootGameObject = Instantiate(droppedItem.lootPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            InstantiateLoot(gameObject.transform.position);
+            Destroy(gameObject);
+        }
+    }
 }
