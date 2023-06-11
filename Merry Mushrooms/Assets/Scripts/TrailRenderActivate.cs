95eb5f69eeb8cddef3fff0e89e7feb601282fc16
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-06-11 12:45:44 -0400

    Changed color of slash

--- Merry Mushrooms/Assets/Scripts/TrailRenderActivate.cs
+++ Merry Mushrooms/Assets/Scripts/TrailRenderActivate.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class TrailRenderActivate : MonoBehaviour
 {
+
+    bool inSlash;
     // Start is called before the first frame update
    
 
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.instance.playerScript.SwordEquipped)
+        if (gameManager.instance.playerScript.SwordEquipped )
         {
 
 
-            if (Input.GetButtonDown("Shoot") )
+            if (Input.GetButtonDown("Shoot") && !inSlash && !gameManager.instance.playerScript.holdingShield)
             {
 
-
-                GetComponent<TrailRenderer>().enabled = true;
+                
+                
                 StartCoroutine(WaitForSlash());
             }
             
@@ -27,9 +29,11 @@ public class TrailRenderActivate : MonoBehaviour
 
     IEnumerator WaitForSlash()
     {
-
+        GetComponent<TrailRenderer>().enabled = true;
+        inSlash = true;
         yield return new WaitForSeconds(1);
         GetComponent<TrailRenderer>().enabled = false;  
+        inSlash = false;
         Debug.Log("Did slash");
         // Instantiate(gameManager.instance.playerScript.SwordList[gameManager.instance.playerScript.selectedSword].slashEffect, slash.transform.position, Camera.main.transform.rotation);
     }
