de08e443de10d94794e1494ce382c13849911696
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-06-13 03:01:36 -0400

    Fixed staff bullet issues

--- Merry Mushrooms/Assets/Scripts/PlayerController.cs
+++ Merry Mushrooms/Assets/Scripts/PlayerController.cs
@@ -392,8 +392,8 @@ public class PlayerController : MonoBehaviour, IDamage, IEffectable, IPhysics
             }
             // Creates bullet object and shoots it towards the center ray of the camera
             GameObject bulletToShoot = Instantiate(staffList[selectedStaff].BulletToShoot, bulletPoint.transform.position, Camera.main.transform.rotation);
-            bulletToShoot.GetComponent<Rigidbody>().velocity = (destination - bulletPoint.transform.position).normalized * speedOfBullet;
-           // Destroy(staffList[selectedStaff].BulletToShoot, 1);
+            bulletToShoot.GetComponent<Rigidbody>().AddForce(bulletPoint.transform.forward * speedOfBullet);
+            //Destroy(staffList[selectedStaff].BulletToShoot, 2,);
 
             //Muzzle Flash
             GameObject muzzle = GameObject.FindGameObjectWithTag("MuzzleFlash");
