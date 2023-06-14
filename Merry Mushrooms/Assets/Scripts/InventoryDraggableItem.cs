d923100829e8c0f6666d4320c587e6294932e012
ItsaGlitch1 <jharneyiiii@gmail.com>
2023-06-13 23:11:20 -0400

    ui prefab

--- Merry Mushrooms/Assets/Scripts/InventoryDraggableItem.cs
+++ Merry Mushrooms/Assets/Scripts/InventoryDraggableItem.cs
@@ -14,6 +14,10 @@ public class InventoryDraggableItem : MonoBehaviour, IBeginDragHandler, IDragHan
     public Image img;
     public Transform newParent;
 
+    //checks
+    public Sprite ManaPot;
+    public Sprite HealthPot;
+
     public void InitializeItem(Item newItem)
     {
         item = newItem;
@@ -32,38 +36,15 @@ public class InventoryDraggableItem : MonoBehaviour, IBeginDragHandler, IDragHan
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            newParent = transform.parent;
-            transform.SetParent(transform.root);
-            transform.SetAsLastSibling();
-            img.raycastTarget = false;
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            Debug.Log(item.name);
-            if (item.name == "Health Potion")
-            {
-                gameManager.instance.playerScript.takeDamage(-30);
-                if (gameManager.instance.playerScript.HP > gameManager.instance.playerScript.maxHP)
-                {
-                    gameManager.instance.playerScript.HP = gameManager.instance.playerScript.maxHP;
-                    gameManager.instance.playerHUD.updatePlayerHealth(0);
-                }
-                gameManager.instance.invManager.RemoveItem(item);
-            }
-            else if (item.name == "Mana Potion")
-            {
-                gameManager.instance.playerScript.MP += 50;
-                gameManager.instance.playerHUD.updatePlayerMana();
-                if (gameManager.instance.playerScript.MP > gameManager.instance.playerScript.maxMP)
-                {
-                    gameManager.instance.playerScript.MP = gameManager.instance.playerScript.maxMP;
+        newParent = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        img.raycastTarget = false;
 
-                }
-                gameManager.instance.invManager.RemoveItem(item);
-            }
-        }
+        //if (Input.GetMouseButtonDown(1))
+        //{
+           
+        //}
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -76,12 +57,35 @@ public class InventoryDraggableItem : MonoBehaviour, IBeginDragHandler, IDragHan
         transform.SetParent(newParent);
         img.raycastTarget = true;
     }
-    //public void OnMouseOver()
-    //{
-    //    if (Input.GetMouseButtonDown(1))
-    //    {
-            
-    //    }
-    //}
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("RightClickTest");
+            if (item.sprite == HealthPot)
+            {
+                Debug.Log("HealthTest");
+                gameManager.instance.playerScript.takeDamage(-30);
+                if (gameManager.instance.playerScript.HP > gameManager.instance.playerScript.maxHP)
+                {
+                    gameManager.instance.playerScript.HP = gameManager.instance.playerScript.maxHP;
+                    gameManager.instance.playerHUD.updatePlayerHealth(0);
+                }
+                gameManager.instance.invManager.RemoveItem(item);
+            }
+            else if (item.sprite = ManaPot)
+            {
+                Debug.Log("ManaTest");
+                gameManager.instance.playerScript.MP += 50;
+                gameManager.instance.playerHUD.updatePlayerMana();
+                if (gameManager.instance.playerScript.MP > gameManager.instance.playerScript.maxMP)
+                {
+                    gameManager.instance.playerScript.MP = gameManager.instance.playerScript.maxMP;
+
+                }
+                gameManager.instance.invManager.RemoveItem(item);
+            }
+        }
+    }
 
 }
