5d3c91d26e161ec975d9ba67822573e8b481f1ee
Carlos Ocampo <caocampo@student.fullsail.edu>
2023-06-20 16:57:54 -0400

    Fixed issue where sword and shield would disappear

--- Merry Mushrooms/Assets/Scripts/gameManager.cs
+++ Merry Mushrooms/Assets/Scripts/gameManager.cs
@@ -176,7 +176,7 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        if (Input.GetButtonDown("Cancel") && activeMenu == null && !playerScript.swordSwung && !playerScript.holdingShield)
         {
             isPaused = !isPaused;
             activeMenu = pauseMenu;
